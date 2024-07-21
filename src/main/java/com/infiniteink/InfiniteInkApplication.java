001fac284f852a1aa86dab477a1355cab7748b42
Aasish Katwal <aasishkatwal@Aasishs-MBP.lan>
2024-07-21 18:33:39 -0500

    first commit

---
+++ src/main/java/com/infiniteink/InfiniteInkApplication.java
@@ -0,0 +1,13 @@
+package com.infiniteink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteInkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfiniteInkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infiniteink/InfiniteInkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infiniteink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteInkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
