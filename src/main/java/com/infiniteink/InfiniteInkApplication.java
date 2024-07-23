8f11a164a68044edb207abc11df4500d4a414887
Aasish <katwalaasish5@gmail.com>
2024-07-23 14:57:46 -0500

    added test 2

--- src/main/java/com/infiniteink/InfiniteInkApplication.java
+++ src/main/java/com/infiniteink/InfiniteInkApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InfiniteInkApplication {
 	
+	// test 2
 	public static void main(String[] args) {
 		SpringApplication.run(InfiniteInkApplication.class, args);
 	}
