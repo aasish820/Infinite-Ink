a9de08a41dfbd98e63165481d2de277d1b75835e
dsatyal1 <dineshsatyal21@gmail.com>
2024-07-24 15:01:44 -0400

    validation added

--- src/main/java/com/infiniteink/entities/Category.java
+++ src/main/java/com/infiniteink/entities/Category.java
@@ -5,12 +5,16 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,20 +23,23 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name="CATEGORY")
+@Table(name = "category")
 public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@NotBlank(message = "Title is manadatory")
+	@Size(min = 1, max = 100, message = "Title must be between 1 and 100  characters")
 	private String title;
-	
-	 @CreationTimestamp
-	 @Column(updatable = false)
-	    private LocalDateTime createdAt;
 
-	    @UpdateTimestamp
-	    private LocalDateTime updatedAt;
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
 
-	    private boolean deleted = false;
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+
+	@JsonIgnore
+	private boolean deleted = false;
 
 }
