c0f02f4fd2959fa05be7db2431ca5cbbda057eac
ParajuliAnusuya <anusuyaparajuli36@gmail.com>
2024-07-24 15:20:02 -0400

    Entity and Repo class added

---
+++ src/main/java/com/infiniteink/entities/Comment.java
@@ -0,0 +1,55 @@
+package com.infiniteink.entities;
+
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.validation.constraints.NotBlank;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "COMMENT")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Comment cannot be empty")
+    private String comment;
+
+//    @ManyToOne
+//    @JoinColumn(name = "post_id", nullable = false)
+//    private Post post_id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user_id;
+
+    @JsonIgnore
+	private LocalDateTime deleted_at;
+	
+	@CreationTimestamp
+	@JsonIgnore
+	private LocalDateTime created_at;
+	
+	@UpdateTimestamp
+	@JsonIgnore
+	private LocalDateTime updated_at;
+
+    // field for soft delete
+    @JsonIgnore
+    private boolean isDeleted = false;
+}
