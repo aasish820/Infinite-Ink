6e4effcebd147a01a8b9e8675bd0ec8478111f33
dsatyal1 <dineshsatyal21@gmail.com>
2024-07-24 16:53:22 -0400

    relation added

--- src/main/java/com/infiniteink/entities/Category.java
+++ src/main/java/com/infiniteink/entities/Category.java
@@ -1,17 +1,23 @@
 package com.infiniteink.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.din.entity.Employee;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -31,13 +37,20 @@ public class Category {
 	@NotBlank(message = "Title is manadatory")
 	@Size(min = 1, max = 100, message = "Title must be between 1 and 100  characters")
 	private String title;
+	
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="post_id", nullable=false)
+	private Post post;
+	
 
 	@CreationTimestamp
-	@Column(updatable = false)
-	private LocalDateTime createdAt;
+	@JsonIgnore
+	private LocalDateTime created_at;
 
 	@UpdateTimestamp
-	private LocalDateTime updatedAt;
+	@JsonIgnore
+	private LocalDateTime updated_at;
 
 	@JsonIgnore
 	private LocalDateTime deleted_at;
