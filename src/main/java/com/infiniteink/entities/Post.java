93a5dbae2d3e66e44857235e872bc51110afc080
vani <vanithamedikonda@gmail.com>
2024-07-24 14:15:20 -0500

    pushing git

--- src/main/java/com/infiniteink/entities/Post.java
+++ src/main/java/com/infiniteink/entities/Post.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,8 +23,11 @@ import lombok.NoArgsConstructor;
 public class Post {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+
 	private Long id;
+	@NotBlank(message="Title is mandatory")
 	private String title;
+	@NotBlank(message="Content is mandatory")
 	private String content;
 	private String image;
 	
