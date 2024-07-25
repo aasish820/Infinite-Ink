cf932a8a6c48e3e8b3660f52bfcd829f3b3cbd2f
vani <vanithamedikonda@gmail.com>
2024-07-25 16:52:48 -0500

    pushing the code exception,repo,service,serviceImpl

---
+++ src/main/java/com/infiniteink/services/impl/PostServiceImpl.java
@@ -0,0 +1,63 @@
+package com.infiniteink.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.infiniteink.entities.Post;
+import com.infiniteink.exceptions.PostNotFoundException;
+import com.infiniteink.repositories.PostRepo;
+import com.infiniteink.services.PostService;
+
+public class PostServiceImpl implements PostService {
+	@Autowired
+	private PostRepo postrepo;
+
+	@Override
+	public List<Post> getAllPosts() {
+		return postrepo.findAllActivePosts();
+	}
+
+	@Override
+	public Post getPostbyId(Long id) {
+		// TODO Auto-generated method stub
+		return postrepo.findActivePostById(id);
+	}
+
+	@Override
+	public List<Post> getPostByUserId(Long userId) {
+		// TODO Auto-generated method stub
+		return postrepo.findActivePostsByUserId(userId);
+	}
+
+	@Override
+	public List<Post> getPostByCategoryId(Long categoryId) {
+		// TODO Auto-generated method stub
+		return postrepo.findActivePostsByCategoryId(categoryId);
+	}
+
+	@Override
+	public Post createPost(Post post) {
+		// TODO Auto-generated method stub
+		return postrepo.save(post);
+	}
+
+	@Override
+	public Post updatePost(Long id, Post postDto) {
+		Post post = postrepo.findActivePostById(id);
+		if (post == null) {
+			throw new PostNotFoundException("Post not found with id " + id);
+		}
+		post.setTitle(postDto.getTitle());
+		post.setContent(postDto.getContent());
+		post.setImage(postDto.getImage());
+
+		return postrepo.save(post);
+	}
+
+	@Override
+	public void deletePost(Long id) {
+		postrepo.softDeletePostById(id);
+	}
+
+}
