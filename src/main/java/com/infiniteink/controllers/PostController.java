39eda817fe496e76b65473323fdbac7f27a17499
vani <vanithamedikonda@gmail.com>
2024-07-30 14:44:33 -0500

    image

--- src/main/java/com/infiniteink/controllers/PostController.java
+++ src/main/java/com/infiniteink/controllers/PostController.java
@@ -1,17 +1,25 @@
 package com.infiniteink.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infiniteink.entities.Post;
@@ -24,6 +32,12 @@ public class PostController {
 	
 	@Autowired
 	private PostServiceImpl postService;
+	
+	@PostMapping("/create")
+    public ResponseEntity<Post> createPost(@ModelAttribute("Post") Post post) {
+        Post createdPost = postService.createPost(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+    }
 	
 	@GetMapping("/fetch")
     public List<Post> getAllPosts() {
@@ -49,13 +63,6 @@ public class PostController {
         return postService.getPostByCategoryId(categoryId);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Post> createPost(@RequestBody Post post) {
-        Post createdPost = postService.createPost(post);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
-    }
-    
-
     @PutMapping("/update/{id}")
     public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post postDto) {
         Post updatedPost = postService.updatePost(id, postDto);
@@ -70,7 +77,37 @@ public class PostController {
         postService.deletePost(id);
         return ResponseEntity.noContent().build();
     }
-}
+    
+   private static final String UPLOAD_DIR="uploads/";
+    @PostMapping("/{id}/uploadImage")
+    public ResponseEntity<Map<String,String>>uploadImage(@PathVariable Long id,@RequestParam("image") MultipartFile imageFile )
+    {
+    	Post postI=postService.getPostbyId(id);
+    	
+    	Path filePath=Paths.get(UPLOAD_DIR+imageFile.getOriginalFilename());
+    	try {
+			Files.createDirectories(filePath.getParent());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	try {
+			Files.write(filePath,imageFile.getBytes());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	String imageUrl="/uploads/"+imageFile.getOriginalFilename();
+    	postI.setImage(imageUrl);
+    	Map<String,String>response=new HashMap<>();
+    	response.put("message", "image upload sucessfull");
+    	response.put("message", imageUrl);
+    	return ResponseEntity.ok(response);
+    }
+    }
+
 	
 
 
--- src/main/java/com/infiniteink/entities/Post.java
+++ src/main/java/com/infiniteink/entities/Post.java
@@ -15,6 +15,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
